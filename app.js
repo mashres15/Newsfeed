efead088aa6862a00df86587040f27313e755ec2
Maniz Shrestha <mashres15@gmail.com>
2017-06-12 21:51:38 -0400

    firstcommit

--- app.js
+++ app.js
@@ -1,136 +1,176 @@
 const app = {
     init(selectors) {
         this.movies = []
+        this.dinos = []
         this.max = 0
-        this.list = document
-            .querySelector(selectors.listSelector)
+        this.movieList = document
+            .querySelector(selectors.movieListSelector)
+        this.dinoList = document
+            .querySelector(selectors.dinoListSelector)
         this.template = document
             .querySelector(selectors.templateSelector)
         document
             .querySelector(selectors.formSelector)
-            .addEventListener('submit', this.addMovieViaForm.bind(this))
+            .addEventListener('submit', this.addFeedViaForm.bind(this))
 
         this.load()
     },
-    
+
     /*-----------------------------------------
     Method to load the movie from localStorage
     ------------------------------------------*/
     load() {
         // Get the JSON string out of localStorage
-        const moviesJSON = localStorage.getItem('movies')
+        const moviesJSON = localStorage.getItem('Movies')
+        const dinosJSON = localStorage.getItem('Dinos')
 
         // Turn that into an array
         const moviesArray = JSON.parse(moviesJSON)
+        const dinosArray = JSON.parse(dinosJSON)
 
         // Set this.movies to that array
         if (moviesArray) {
             moviesArray
                 .reverse()
-                .map(this.addMovie.bind(this))
+                .map(this.addFeed.bind(this))
+        }
+
+        if (dinosArray){
+            dinosArray
+                .reverse()
+                .map(this.addFeed.bind(this))
         }
     },
 
-    addMovie(movie) {
-        const listItem = this.renderListItem(movie)
-        this.list
-            .insertBefore(listItem, this.list.firstChild)
-
-        if(movie.id > this.max){
-            this.max = movie.id
+    addFeed(feed) {
+        const listItem = this.renderListItem(feed)
+        if(feed.category === 'Movies'){
+            list = this.movieList
+            arr = this.movies
         }
-        this.movies.unshift(movie)
+        else{
+            list = this.dinoList
+            arr = this.dinos
+        }
+
+        list.insertBefore(listItem, list.firstChild)
+        arr.unshift(feed)
+        
+
+        if(feed.id > this.max){
+            this.max = feed.id
+        }
+
         this.save()
+       
     },
 
-    addMovieViaForm(ev) {
+    addFeedViaForm(ev) {
         ev.preventDefault()
         const f = ev.target
-        //console.log(f.movieName.value)
-        const movie = {
-            id: this.max + 1,
-            name: f.movieName.value,
-            year: f.year.value,
-            promote: false,
-        }
-
-        this.addMovie(movie)
+        if(f.category.value === 'Movies'){
+            const movie = {
+                id: this.max + 1,
+                name: f.feedName.value,
+                category: f.category.value,
+                promote: false,
+            }
+            this.addFeed(movie)
+            }
+        else{
+            const dino = {
+                id: this.max + 1,
+                name: f.feedName.value,
+                category: f.category.value,
+                promote: false,
+            }
+            this.addFeed(dino)
+            }
         f.reset()
     },
 
     save() {
         localStorage
-            .setItem('movies', JSON.stringify(this.movies))
+            .setItem('Movies', JSON.stringify(this.movies))
+
+        localStorage
+            .setItem('Dinos', JSON.stringify(this.dinos))
 
     },
 
-    renderListItem(movie) {
+    renderListItem(feed) {
         const item = this.template.cloneNode(true)
         item.classList.remove('template')
-        item.dataset.id = movie.id
-        
-        if(movie.promote){
+        item.dataset.id = feed.id
+
+        if(feed.promote){
             item.classList.add('promote')
         }
-        
-        item
-            .querySelector('.movie-name')
-            .textContent = movie.name
 
         item
-            .querySelector('.movie-year')
-            .textContent = "Category: "+movie.year
+            .querySelector('.feed-name')
+            .textContent = feed.name
+
+        item
+            .querySelector('.feed-category')
+            .textContent = "Category: "+feed.category
 
         item
             .querySelector('button.remove')
-            .addEventListener('click', this.removeMovie.bind(this))
+            .addEventListener('click', this.removeFeed.bind(this, feed))
 
         item
             .querySelector('button.edit')
-            .addEventListener('click', this.editText.bind(this))
+            .addEventListener('click', this.editText.bind(this,feed))
 
         item
             .querySelector('button.fav')
-            .addEventListener('click', this.promoteMovie.bind(this, movie))
+            .addEventListener('click', this.promoteFeed.bind(this, feed))
 
         item
             .querySelector('button.up')
-            .addEventListener('click', this.moveUp.bind(this, movie))
+            .addEventListener('click', this.moveUp.bind(this, feed))
 
         item
             .querySelector('button.down')
-            .addEventListener('click', this.moveDown.bind(this, movie))
+            .addEventListener('click', this.moveDown.bind(this, feed))
 
         return item
     },
 
-    editText(ev){
+    editText(feed, ev){
         const editbutton = ev.target
-        const listItem = ev.target.closest('.movie')
-        const movieName = listItem.children[2]
-        if(movieName.isContentEditable){
-            //console.log("in true")
-            movieName.contentEditable = false
+        const listItem = ev.target.closest('.feed')
+        const feedName = listItem.children[2]
+        if(feedName.isContentEditable){
+
+            feedName.contentEditable = false
             editbutton.classList.add('fa-pencil-square-o')
             editbutton.classList.remove('fa-check')
             editbutton.classList.add('warning')
             editbutton.classList.remove('success')
 
+            if(feed.category === 'Movies'){
+                const arr = this.movies
+                }
+            else{
+                const arr = this.dinos
+                }
             // Find the movie in the array, and remove it
-            for (let i = 0; i < this.movies.length; i++) {
-                const currentId = this.movies[i].id.toString()
+            for (let i = 0; i < arr.length; i++) {
+                const currentId = arr[i].id.toString()
                 if (listItem.dataset.id === currentId) {
-                    this.movies[i].name = movieName.innerHTML
+                    arr[i].name = feedName.innerHTML
                     break
                 }
-            }
-            //console.log(this.movies)
+            } 
+
             this.save()
         }
         else{
             //console.log("false")
-            movieName.contentEditable = true
-            movieName.focus()
+            feedName.contentEditable = true
+            feedName.focus()
             editbutton.classList.add('fa-check')
             editbutton.classList.remove('fa-pencil-square-o')
             editbutton.classList.remove('warning')
@@ -138,23 +178,30 @@ const app = {
         }
     },
 
-    promoteMovie(movie, ev){
-        const listItem = ev.target.closest('.movie')
-        movie.promote = !movie.promote
+    promoteFeed(feed, ev){
+        const listItem = ev.target.closest('.feed')
+        feed.promote = !feed.promote
 
         // toggle promote
         listItem.classList.toggle('promote')
         this.save()
     },
 
-    removeMovie(ev) {
-        const listItem = ev.target.closest('.movie')
+    removeFeed(feed, ev) {
+        const listItem = ev.target.closest('.feed')
 
-        // Find the movie in the array, and remove it
-        for (let i = 0; i < this.movies.length; i++) {
-            const currentId = this.movies[i].id.toString()
+        if(feed.category === 'Movies'){
+            const arr = this.movies
+            }
+
+        else{
+            const arr = this.dinos
+            }
+        // Find the feed in the array, and remove it
+        for (let i = 0; i < arr.length; i++) {
+            const currentId = arr[i].id.toString()
             if (listItem.dataset.id === currentId) {
-                this.movies.splice(i, 1)
+                arr.splice(i, 1)
                 break
             }
         }
@@ -163,41 +210,55 @@ const app = {
         this.save()
     },
 
-    moveUp(movie, ev) {
-        const li = ev.target.closest('.movie')
+    moveUp(feed, ev) {
+        const li = ev.target.closest('.feed')
+        if(feed.category === 'Movies'){
+            const arr = this.movies
+            list = this.movieList
+        }
+        else{
+            const arr = this.dinos 
+            list = this.dinoList
+        }
 
-        const index = this.movies.findIndex((currentMovie) => {
-            return currentMovie.id === movie.id
+        const index = arr.findIndex((currentFeed) => {
+            return currentFeed.id === feed.id
         })
-        //console.log(this.movies)
-        //console.log(index)
 
         if (index > 0) {
             // Changing the DOM
-            this.list.insertBefore(li, li.previousElementSibling)
+            list.insertBefore(li, li.previousElementSibling)
 
-            //Swap the movies in the array
-            const previousMovie = this.movies[index - 1]
-            this.movies[index - 1] = movie
-            this.movies[index] = previousMovie
+            //Swap the feed in the array
+            const previousFeed = arr[index - 1]
+            arr[index - 1] = feed
+            arr[index] = previousFeed
 
             this.save()
         }
     },
 
-    moveDown(movie, ev) {
-        const li = ev.target.closest('.movie')
+    moveDown(feed, ev) {
+        const li = ev.target.closest('.feed')
+        if(feed.category === 'Movies'){
+            const arr = this.movies
+            list = this.movieList
+        }
+        else{
+            const arr = this.dinos 
+            list = this.dinoList
+        }
 
-        const index = this.movies.findIndex((currentMovie) => {
-            return currentMovie.id === movie.id
+        const index = this.movies.findIndex((currentFeed) => {
+            return currentFeed.id === feed.id
         })
-        
-        if (index < this.movies.length - 1) {
-            this.list.insertBefore(li.nextSibling, li)
 
-            const nextMovie = this.movies[index + 1]
-            this.movies[index + 1] = movie
-            this.movies[index] = nextMovie
+        if (index < arr.length - 1) {
+            list.insertBefore(li.nextSibling, li)
+
+            const nextFeed = arr[index + 1]
+            arr[index + 1] = feed
+            arr[index] = nextFeed
 
             this.save()
         }
@@ -205,9 +266,10 @@ const app = {
 }
 
 app.init({
-    formSelector: '#movie-form',
-    listSelector: '#movie-list',
-    templateSelector: '.movie.template',
+    formSelector: '#feed-form',
+    movieListSelector: '#movie-list',
+    dinoListSelector: '#dino-list',
+    templateSelector: '.feed.template',
 })
 
 
